b91bdc3917ff957a3aee4d61387af33e62f5fcf7
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 18:31:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,13 +18,10 @@ function whileLoop(n) {
     return "done";
   }
 
-function doWhileLoop(array){
-function maybeTrue() {
-  return Math.random() >= 0.5;
-}
-do{
+function doWhileLoop(array) {
+  do{
   array.pop();
 }
-while (array.length > 0 && maybeTrue())
-return array ;
+  while (array.length > 0 && maybeTrue())
+  return array;
 }
